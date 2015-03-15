95e776a51659b5e04e9d5ee02d6661e6022d4cd9
Johannes Haseitl <johannes@undpaul.de>
2015-03-15 17:15:52 +0100

    Addd properties to RaceResultItem

--- src/undpaul/MarioKartBundle/Entity/RaceResultItem.php
+++ src/undpaul/MarioKartBundle/Entity/RaceResultItem.php
@@ -15,11 +15,6 @@ class RaceResultItem
     private $id;
 
     /**
-     * @var integer
-     */
-    private $delta;
-
-    /**
      * @var \undpaul\MarioKartBundle\Entity\Race
      */
     private $race;
@@ -38,29 +33,6 @@ class RaceResultItem
     public function getId()
     {
         return $this->id;
-    }
-
-    /**
-     * Set delta
-     *
-     * @param integer $delta
-     * @return RaceResultItem
-     */
-    public function setDelta($delta)
-    {
-        $this->delta = $delta;
-
-        return $this;
-    }
-
-    /**
-     * Get delta
-     *
-     * @return integer
-     */
-    public function getDelta()
-    {
-        return $this->delta;
     }
 
     /**
@@ -107,5 +79,117 @@ class RaceResultItem
     public function getUser()
     {
         return $this->user;
+    }
+    /**
+     * @var integer
+     */
+    private $pos_rel;
+
+    /**
+     * @var integer
+     */
+    private $pos_abs;
+
+    /**
+     * @var integer
+     */
+    private $pts_rel;
+
+    /**
+     * @var integer
+     */
+    private $pts_abs;
+
+
+    /**
+     * Set pos_rel
+     *
+     * @param integer $posRel
+     * @return RaceResultItem
+     */
+    public function setPosRel($posRel)
+    {
+        $this->pos_rel = $posRel;
+
+        return $this;
+    }
+
+    /**
+     * Get pos_rel
+     *
+     * @return integer 
+     */
+    public function getPosRel()
+    {
+        return $this->pos_rel;
+    }
+
+    /**
+     * Set pos_abs
+     *
+     * @param integer $posAbs
+     * @return RaceResultItem
+     */
+    public function setPosAbs($posAbs)
+    {
+        $this->pos_abs = $posAbs;
+
+        return $this;
+    }
+
+    /**
+     * Get pos_abs
+     *
+     * @return integer 
+     */
+    public function getPosAbs()
+    {
+        return $this->pos_abs;
+    }
+
+    /**
+     * Set pts_rel
+     *
+     * @param integer $ptsRel
+     * @return RaceResultItem
+     */
+    public function setPtsRel($ptsRel)
+    {
+        $this->pts_rel = $ptsRel;
+
+        return $this;
+    }
+
+    /**
+     * Get pts_rel
+     *
+     * @return integer 
+     */
+    public function getPtsRel()
+    {
+        return $this->pts_rel;
+    }
+
+    /**
+     * Set pts_abs
+     *
+     * @param integer $ptsAbs
+     * @return RaceResultItem
+     */
+    public function setPtsAbs($ptsAbs)
+    {
+        $this->pts_abs = $ptsAbs;
+
+        return $this;
+    }
+
+    /**
+     * Get pts_abs
+     *
+     * @return integer 
+     */
+    public function getPtsAbs()
+    {
+        return $this->pts_abs;
     }
 }
