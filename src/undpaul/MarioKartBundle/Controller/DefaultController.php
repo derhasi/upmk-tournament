9255fe184d2b4c5a187b5406e83110ed81426d71
Johannes Haseitl <johannes@undpaul.de>
2015-03-17 12:42:43 +0100

    Updated homepage to link to players and tournaments

--- src/undpaul/MarioKartBundle/Controller/DefaultController.php
+++ src/undpaul/MarioKartBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->redirect($this->generateUrl('upmk_legacy'));
+        return $this->render('undpaulMarioKartBundle:Default:index.html.twig');
     }
 }
